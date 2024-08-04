6f007b78ad74032377b43661ee0f59e906f8e187
Ayron Sanfra <ayron.marinho@estudante.ufla.br>
2024-08-04 14:38:59 -0300

    fix: landing page style

--- view/juba/src/pages/LandingPage/LandingPage.tsx
+++ view/juba/src/pages/LandingPage/LandingPage.tsx
@@ -1,33 +1,44 @@
 import React, { useEffect } from "react";
-import "./LandingPage.css"; 
+import "./LandingPage.css";
 import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  useEffect(() => {}, []);
 
-    useEffect(() => {
-    }, []);
-
-    return (
-        <div className="App">
-            <header className="header">
-                <div className="logo-lp">
-                    <img src="./logo-cadastro.png" alt="JUBA Logo" className="logo-img"/>
-                </div>
-            </header>
-            <section className="hero">
-                <p className="impact-text">Transforme sua gestão financeira com facilidade. Controle total, decisões inteligentes.</p>
-                <div className="buttons">
-                <button className="btn" onClick={() => navigate("/login")}>Login</button>
-                <button className="btn" onClick={() => navigate("/register")}>Criar conta</button>
-                </div>
-            </section>
-            <section className="benefits">
-                <div className="benefit-card">Vantagem 1</div>
-                <div className="benefit-card">Vantagem 2</div>
-                <div className="benefit-card">Vantagem 3</div>
-            </section>
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo-lp">
+          <img src="./logo-cadastro.png" alt="JUBA Logo" className="logo-img" />
         </div>
-    );
+      </header>
+      <section className="hero">
+        <p className="impact-text">
+          Transforme sua gestão financeira com facilidade. Controle total,
+          decisões inteligentes.
+        </p>
+        <div className="buttons">
+          <button className="btn" onClick={() => navigate("/login")}>
+            Login
+          </button>
+          <button className="btn" onClick={() => navigate("/register")}>
+            Criar conta
+          </button>
+        </div>
+      </section>
+      <section className="benefits">
+        <div className="benefit-card">
+          <b>Mais controle de seus gastos</b>
+        </div>
+        <div className="benefit-card">
+          <b>Análise completa de transações</b>
+        </div>
+        <div className="benefit-card">
+          <b>Categorização de despesas</b>
+        </div>
+      </section>
+    </div>
+  );
 }
