f45a29361b08234430ec1e654ca2edc47fa95c81
Luiza Garcia <luizaoliveiragso@gmail.com>
2024-08-04 17:49:37 -0300

    ajustes

--- Banco de dados/bd.sql
+++ Banco de dados/bd.sql
@@ -1,75 +1,60 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE IF NOT EXISTS bd_engsoftware DEFAULT CHARACTER SET utf8mb4 DEFAULT ENCRYPTION='N';
+USE bd_engsoftware;
 
-CREATE DATABASE IF NOT EXISTS `bd_engsoftware` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bd_engsoftware`;
+CREATE TABLE IF NOT EXISTS categoria (
+  id int NOT NULL AUTO_INCREMENT,
+  idUsuario int NOT NULL,
+  nome varchar(45) NOT NULL,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  KEY id (id),
+  KEY createdAt (createdAt),
+  KEY idUsuario (idUsuario),
+  CONSTRAINT fk_categoria_usuario1 FOREIGN KEY (idUsuario) REFERENCES usuario (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `categoria` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `idUsuario` int NOT NULL,
-  `nome` varchar(45) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `id` (`id`),
-  KEY `createdAt` (`createdAt`),
-  KEY `idUsuario` (`idUsuario`),
-  CONSTRAINT `fk_categoria_usuario1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb3;
+CREATE TABLE IF NOT EXISTS categoriatransacao (
+  idCategoria int NOT NULL,
+  idTransacao int NOT NULL,
+  valor float NOT NULL,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (idCategoria,idTransacao),
+  KEY fk_categoriaTransacao_has_transacao_transacao1_idx (idTransacao),
+  KEY fk_categoriaTransacao_has_transacao_categoriaTransacao_idx (idCategoria),
+  KEY createdAt (createdAt),
+  CONSTRAINT fk_categoriaTransacao_has_transacao_categoriaTransacao FOREIGN KEY (idCategoria) REFERENCES categoria (id),
+  CONSTRAINT fk_categoriaTransacao_has_transacao_transacao1 FOREIGN KEY (idTransacao) REFERENCES transacao (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `categoriatransacao` (
-  `idCategoria` int NOT NULL,
-  `idTransacao` int NOT NULL,
-  `valor` double(15,2) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`idCategoria`,`idTransacao`),
-  KEY `fk_categoriaTransacao_has_transacao_transacao1_idx` (`idTransacao`),
-  KEY `fk_categoriaTransacao_has_transacao_categoriaTransacao_idx` (`idCategoria`),
-  KEY `createdAt` (`createdAt`),
-  CONSTRAINT `fk_categoriaTransacao_has_transacao_categoriaTransacao` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`id`),
-  CONSTRAINT `fk_categoriaTransacao_has_transacao_transacao1` FOREIGN KEY (`idTransacao`) REFERENCES `transacao` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+CREATE TABLE IF NOT EXISTS transacao (
+  id int NOT NULL AUTO_INCREMENT,
+  idUsuario int NOT NULL,
+  descricao varchar(250) NOT NULL,
+  data datetime NOT NULL,
+  valor float NOT NULL,
+  tipo char(1) NOT NULL,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id,idUsuario),
+  KEY fk_transacao_usuario1_idx (idUsuario),
+  KEY data (data),
+  KEY tipo (tipo),
+  KEY id (id),
+  KEY createdAt (createdAt),
+  CONSTRAINT fk_transacao_usuario1 FOREIGN KEY (idUsuario) REFERENCES usuario (id)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS `transacao` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `idUsuario` int NOT NULL,
-  `descricao` varchar(250) NOT NULL,
-  `data` datetime NOT NULL,
-  `valor` double(15,2) NOT NULL,
-  `tipo` char(1) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`,`idUsuario`),
-  KEY `fk_transacao_usuario1_idx` (`idUsuario`),
-  KEY `data` (`data`),
-  KEY `tipo` (`tipo`),
-  KEY `id` (`id`),
-  KEY `createdAt` (`createdAt`),
-  CONSTRAINT `fk_transacao_usuario1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb3;
-
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(60) NOT NULL,
-  `senha` varchar(60) NOT NULL,
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `email` (`email`),
-  KEY `createdAt` (`createdAt`),
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+CREATE TABLE IF NOT EXISTS usuario (
+  id int NOT NULL AUTO_INCREMENT,
+  nome varchar(100) NOT NULL,
+  email varchar(60) NOT NULL,
+  senha varchar(60) NOT NULL,
+  createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  KEY email (email),
+  KEY createdAt (createdAt),
+  KEY id (id)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
