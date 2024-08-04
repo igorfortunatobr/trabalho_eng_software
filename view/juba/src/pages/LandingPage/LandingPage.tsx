6be7f6d907352e27f2b53a60d4e1bc89bf90d336
Luiza Garcia <luizaoliveiragso@gmail.com>
2024-08-04 15:05:35 -0300

    landingpage

--- view/juba/src/pages/LandingPage/LandingPage.tsx
+++ view/juba/src/pages/LandingPage/LandingPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./LandingPage.css";
 import { useNavigate } from "react-router-dom";
+import Footer from "../../components/footer/Footer";
 
 export default function LandingPage() {
   const navigate = useNavigate();
@@ -9,36 +10,43 @@ export default function LandingPage() {
 
   return (
     <div className="App">
-      <header className="header">
-        <div className="logo-lp">
-          <img src="./logo-cadastro.png" alt="JUBA Logo" className="logo-img" />
-        </div>
-      </header>
-      <section className="hero">
-        <p className="impact-text">
-          Transforme sua gestão financeira com facilidade. Controle total,
-          decisões inteligentes.
-        </p>
-        <div className="buttons">
-          <button className="btn" onClick={() => navigate("/login")}>
-            Login
-          </button>
-          <button className="btn" onClick={() => navigate("/register")}>
-            Criar conta
-          </button>
-        </div>
-      </section>
-      <section className="benefits">
-        <div className="benefit-card">
-          <b>Mais controle de seus gastos</b>
-        </div>
-        <div className="benefit-card">
-          <b>Análise completa de transações</b>
-        </div>
-        <div className="benefit-card">
-          <b>Categorização de despesas</b>
-        </div>
-      </section>
+        <header className="header">
+            <div className="logo-lp">
+            <img src="./logo-cadastro.png" alt="JUBA Logo" className="logo-img" />
+            </div>
+        </header>
+        <section className="hero">
+            <p className="impact-text">
+            Transforme sua gestão financeira com facilidade. Controle total,
+            decisões inteligentes.
+            </p>
+            <div className="buttons">
+            <button className="btn" onClick={() => navigate("/login")}>
+                Login
+            </button>
+            <button className="btn" onClick={() => navigate("/register")}>
+                Criar conta
+            </button>
+            </div>
+        </section>
+        <section className="benefits">
+            <div className="benefit-card">
+                <b>Organização e Controle Detalhado das Finanças</b>
+                <p>Com o Juba, você pode registrar e categorizar todas as suas transações financeiras, proporcionando uma visão clara de onde seu dinheiro está sendo alocado. Mantenha suas finanças organizadas e tome decisões financeiras mais informadas com facilidade.</p>
+                <img src="./organizacao.png" alt="Segurança e Personalização" className="card-image" />
+            </div>
+            <div className="benefit-card">
+                <b>Relatórios Completos e Personalizados</b>
+                <p>Acesse uma gama de relatórios que oferecem uma visão abrangente do seu comportamento financeiro. Identifique padrões de gastos e áreas para economizar com relatórios detalhados, como "Gastos por Categoria" e "Transações por Categoria".</p>
+                <img src="./relatorios.png" alt="Segurança e Personalização" className="card-image" />
+            </div>
+                <div className="benefit-card">
+                <b>Segurança e Personalização</b>
+                <p>Gerencie suas finanças de forma segura com contas protegidas por senha. O Juba permite total flexibilidade para adicionar, editar ou remover transações e categorias conforme suas necessidades, garantindo que o sistema sempre reflita sua realidade financeira atual.</p>
+                <img src="./seguranca.png" alt="Segurança e Personalização" className="card-image" />
+            </div>
+        </section>
+        <Footer />
     </div>
   );
 }
