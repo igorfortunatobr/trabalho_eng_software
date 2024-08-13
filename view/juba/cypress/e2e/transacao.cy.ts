613ff42cea4f74de65cee5a0f52bfe46d7fdb35c
Igor Fortunato <54378562+igorfortunatobr@users.noreply.github.com>
2024-08-13 19:17:07 -0300

    Ajustes testes

--- view/juba/cypress/e2e/transacao.cy.ts
+++ view/juba/cypress/e2e/transacao.cy.ts
@@ -9,7 +9,30 @@ describe('TransacaoCRUD', () => {
 
     it('Deve carregar a página de transações corretamente', () => {
         cy.contains('Listagem de Transações').should('be.visible');
-        cy.get('button').contains('Nova Transação').should('be.visible');
+        cy.get('button').contains('Nova Transação').click();
+        cy.get('input#descricao').type('Transação Teste 1');
+        const currentDate = new Date().toISOString().split('T')[0];
+        cy.get('input#data').type(currentDate);
+        cy.get('input#despesa').check();
+        // Adicionar uma categoria
+        cy.get('select#novaCategoria').then($select => {
+            const options = $select.find('option');
+            cy.wrap($select).select(options.eq(2).val());
+        });
+        cy.get('input#novaCategoria').type('20.00');
+        cy.get('button').contains('Adicionar Categoria').click();
+        
+        // Adicionar outra categoria
+        cy.get('select#novaCategoria').then($select => {
+            const options = $select.find('option');
+            cy.wrap($select).select(options.eq(3).val());
+        });
+        cy.get('input#novaCategoria').type('50.00');
+        cy.get('button').contains('Adicionar Categoria').click();
+
+        cy.get('button').contains('Salvar').click();
+        cy.contains('Transação Teste 1').should('be.visible');
+        cy.get('card').contains('R$ 70,00').should('be.visible');
     });
 
 });
