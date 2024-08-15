99377091c3c0c496a7d86f0c73565fc6454d2ba6
Luiza Garcia <luizaoliveiragso@gmail.com>
2024-08-15 19:54:03 -0300

    ajustes transacao

--- view/juba/cypress/support/commands.ts
+++ view/juba/cypress/support/commands.ts
@@ -5,6 +5,7 @@ declare global {
     interface Chainable {
       realizaLogin(): Chainable<void>
       verificaRegistro(): Chainable<void>
+      cadastraCategorias(nomeCategoria: string): Chainable<void>
     }
   }
 }
@@ -12,7 +13,7 @@ declare global {
 Cypress.Commands.add('verificaRegistro', () => {
   cy.request({
     method: 'POST',
-    url: 'http://localhost:8098/auth/login',
+    url: `${Cypress.env('backendUrl')}/auth/login`,
     failOnStatusCode: false, // Não falha se o status for diferente de 2xx
     body: {
       email: 'novo.usuario@example.com',
@@ -21,7 +22,7 @@ Cypress.Commands.add('verificaRegistro', () => {
   }).then((response) => {
     if (response.status === 401) {
       // Se o login falhar com 401, o usuário não existe, então o registramos
-      cy.request('POST', 'http://localhost:8098/usuarios/register', {
+      cy.request('POST', `${Cypress.env('backendUrl')}/usuarios/register`, {
         nome: 'Novo Usuário',
         email: 'novo.usuario@example.com',
         senha: 'senha123'
@@ -34,7 +35,7 @@ Cypress.Commands.add('verificaRegistro', () => {
 
 Cypress.Commands.add('realizaLogin', () => {
   cy.session('novo.usuario@example.com', () => {
-    cy.request('POST', 'http://localhost:8098/auth/login', {
+    cy.request('POST', `${Cypress.env('backendUrl')}/auth/login`, {
       email: 'novo.usuario@example.com',
       senha: 'senha123'
     }).then((response) => {
@@ -43,4 +44,24 @@ Cypress.Commands.add('realizaLogin', () => {
       window.localStorage.setItem('token', token); // Salva o token no localStorage
     });
   });
+});
+
+Cypress.Commands.add('cadastraCategorias', (nomeCategoria) => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.env('backendUrl')}/categorias`,
+        headers: {
+          Authorization: `${token}`, // Adiciona o token no cabeçalho
+        },
+        body: {
+          nome: nomeCategoria,
+        }
+      }).then((response) => {
+        expect(response.status).to.eq(201); // Verifica se o cadastro foi bem-sucedido
+      });
+    } else {
+      throw new Error('Token não encontrado no localStorage');
+    }
 });
