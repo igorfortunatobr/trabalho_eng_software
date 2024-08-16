c7d6e1d724042aff53789ca655cbb697c56d9ca2
Luiza Garcia <luizaoliveiragso@gmail.com>
2024-08-16 17:31:32 -0300

    ajuste

--- view/juba/cypress/e2e/transacao.cy.ts
+++ view/juba/cypress/e2e/transacao.cy.ts
@@ -1,3 +1,5 @@
+import { wait } from "@testing-library/user-event/dist/utils";
+
 describe('TransacaoCRUD', () => {
   
     beforeEach(() => {
@@ -20,8 +22,7 @@ describe('TransacaoCRUD', () => {
         cy.get('input#data').type(currentDate);
         cy.get('input#despesa').check();
         
-        // Adicionar a primeira categoria
-        // Adicionar a primeira categoria
+        // Adicionar a primeira categoria à transação
         let firstValue = '';
         cy.get('select#novaCategoria').then($select => {
             // Encontra todas as opções que não têm o valor vazio
@@ -63,15 +64,16 @@ describe('TransacaoCRUD', () => {
         cy.get('button').contains('Adicionar Categoria').click();
 
         cy.get('button').contains('Salvar').click();
+        //Espera 10 segundos para a tela carregar
         cy.wait(10);
         cy.contains('Transação Teste 1').should('be.visible');
-        //cy.get('card').contains('R$ 70,00').should('be.visible');
     });
 
     it('Inserir uma nova transação do tipo RECEITA', () => {
         cy.contains('Listagem de Transações').should('be.visible');
         cy.get('button').contains('Nova Transação').click();
         cy.get('input#descricao').type('Transação Teste 2');
+
         const currentDate = new Date().toISOString().split('T')[0];
         cy.get('input#data').type(currentDate);
         cy.get('input#receita').check();
@@ -120,13 +122,14 @@ describe('TransacaoCRUD', () => {
         cy.get('button').contains('Salvar').click();
         cy.wait(10);
         cy.contains('Transação Teste 2').should('be.visible');
-        //cy.get('card').contains('R$ 360,00').should('be.visible');
     });
 
     it('Inserir e editar uma nova transação', () => {
+
         cy.contains('Listagem de Transações').should('be.visible');
         cy.get('button').contains('Nova Transação').click();
         cy.get('input#descricao').type('Transação Teste 3');
+
         const currentDate = new Date().toISOString().split('T')[0];
         cy.get('input#data').type(currentDate);
         cy.get('input#receita').check();
@@ -181,23 +184,86 @@ describe('TransacaoCRUD', () => {
         cy.get('input#despesa').check();
         cy.get('button').contains('Salvar').click();
         cy.contains('Transação Teste EDITADA').should('be.visible');
-
-        //cy.get('card').contains('R$ 360,00').should('be.visible');
     });
 
     it('Deve excluir uma transação já existente', () => {
-    // Verifica se a página de categorias foi carregada corretamente
-    cy.contains('Transação Teste EDITADA').should('be.visible');
+        // Verifica se a página de categorias foi carregada corretamente
+        cy.contains('Transação Teste EDITADA').should('be.visible');
 
-    cy.contains('Transação Teste 1').parent('tr').find('.btn-danger').click();
-    cy.contains('Transação Teste 2').parent('tr').find('.btn-danger').click();
-    cy.contains('Transação Teste EDITADA').parent('tr').find('.btn-danger').click();
+        cy.contains('Transação Teste 1').parent('tr').find('.btn-danger').click();
+        cy.contains('Transação Teste 2').parent('tr').find('.btn-danger').click();
+        cy.contains('Transação Teste EDITADA').parent('tr').find('.btn-danger').click();
 
-    // Verifica se a categoria editada aparece na lista
-    cy.contains('Transação Teste 1').should('not.exist');
-    cy.contains('Transação Teste 2').should('not.exist');
-    cy.contains('Transação Teste EDITADA').should('not.exist');
-    //cy.contains('Transação excluída com sucesso');
-  });
+        // Verifica se a categoria editada aparece na lista
+        cy.contains('Transação Teste 1').should('not.exist');
+        cy.contains('Transação Teste 2').should('not.exist');
+        cy.contains('Transação Teste EDITADA').should('not.exist');
+    });
+
+    it('Deve impedir inserção de transação sem categoria', () => {
+
+        cy.contains('Listagem de Transações').should('be.visible');
+        cy.get('button').contains('Nova Transação').click();
+        cy.get('input#descricao').type('Transação Teste 2');
+        cy.get('input#despesa').check();
+
+        // Tentar salvar sem adicionar categoria
+        cy.get('button').contains('Salvar').click();
+        cy.wait(10);
+        cy.contains('Adicione ao menos uma categoria').should('be.visible');
+    });
+
+    it('Deve impedir inserção de transação sem descrição', () => {
+
+        cy.contains('Listagem de Transações').should('be.visible');
+        cy.get('button').contains('Nova Transação').click();
+        cy.get('input#despesa').check();
+        
+        // Adicionar uma categoria
+        let firstValue = '';
+        cy.get('select#novaCategoria').then($select => {
+            // Encontra todas as opções que não têm o valor vazio
+            const validOptions = $select.find('option').filter((index, option) => {
+                return (option as HTMLOptionElement).value !== '';
+            });
+
+            // Seleciona a primeira opção válida
+            if (validOptions.length > 0) {
+                firstValue = (validOptions[0] as HTMLOptionElement).value;
+                cy.wrap($select).select(firstValue);
+            }
+        });
+
+        cy.get('input#novaCategoria').type('11.00');
+        cy.wait(10);
+        cy.get('button').contains('Adicionar Categoria').click();
+
+        // Tentar salvar sem descrição
+        cy.get('button').contains('Salvar').click();
+        cy.wait(10);
+        cy.contains('Descrição é obrigatória').should('be.visible');
+    });
+
+    // it('Deve verificar os totalizadores após inserir uma transação', () => {
+    //     cy.contains('Listagem de Transações').should('be.visible');
+    //     cy.get('button').contains('Nova Transação').click();
+    //     cy.get('input#descricao').type('Transação Teste 2');
+    //     cy.get('input#data').type(new Date().toISOString().split('T')[0]);
+    //     cy.get('input#receita').check();
+        
+    //     // Adicionar categorias
+    //     cy.get('select#novaCategoria').select('Categoria 1');
+    //     cy.get('input#novaCategoria').type('200.00');
+    //     cy.get('button').contains('Adicionar Categoria').click();
+    //     cy.get('select#novaCategoria').select('Categoria 2');
+    //     cy.get('input#novaCategoria').type('50.00');
+    //     cy.get('button').contains('Adicionar Categoria').click();
+
+    //     cy.get('button').contains('Salvar').click();
+        
+    //     // Verifica os totalizadores após a inserção
+    //     cy.get('.card-title').contains('Total Receitas').parent().find('.card-text').should('contain', 'R$ 250,00');
+    //     cy.get('.card-title').contains('Saldo').parent().find('.card-text').should('contain', 'R$ 250,00');
+    // });
 
 });
